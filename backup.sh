fcabfd4e3b0352bf06ad9ed9e101ee2310bba3d7
Son9o <Son9o@users.noreply.github.com>
2016-07-21 18:35:05 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,19 +3,24 @@
 PASSWORD_MYSQL=your_root_mysql_pass
 DATE=`date +%d-%m-%y`
 NAME=`hostname`_$DATE
+LOGFILE=/root/backup_$NAME.log
+exec > $LOGFILE
+exec 2>&1
 cd /
+echo "Initailised Logfile on $DATE" >> $LOGFILE
 mysqldump -u root -p$PASSWORD_MYSQL --events --all-databases | gzip > /root/all_databases_$DATE.sql.gz
-tar -cvpzf $NAME.tar.gz --exclude=/$NAME.tar.gz --exclude=/proc --exclude=/sys --exclude=/mnt --exclude=/media --exclude=/run --exclude=/dev --exclude=/lost+found --exclude=/tmp --exclude=/home/son9o/steamcmd --exclude=/home/transmission/Downloads --exclude=/var/lib/transmission/Downloads --exclude=/root/backup.log / > /root/backup.log
-echo "Initailised megastats.log on $DATE" > /root/megastats.log
-echo "Output from upload:" >> /root/megastats.log
-/usr/local/bin/megaput $NAME.tar.gz > /root/megastats.log
-echo "Disk space check:" >> /root/megastats.log
+tar -cvpzf $NAME.tar.gz --exclude=/$NAME.tar.gz --exclude=/proc --exclude=/sys --exclude=/mnt --exclude=/media --exclude=/run --exclude=/dev --exclude=/lost+found --exclude=/tmp --exclude=/home/son9o/steamcmd --exclude=/home/transmission/Downloads --exclude=/var/lib/transmission/Downloads --exclude=/root/backup_filelist.log / > /root/backup_filelist.log
+echo "Initailising Megatools operations:"
+echo "Output from upload:"
+/usr/local/bin/megaput $NAME.tar.gz > 
+echo "Disk space check:" 
 /usr/local/bin/megadf --gb | tail -n3 >> /root/megastats.log
-echo "File List:" >> /root/megastats.log
+echo "File List:" 
 /usr/local/bin/megals -hl | grep -i $NAME >> /root/megastats.log
+echo "Login credentails used:" 
 cat /root/.megarc >> /root/megastats.log
-cat /root/megastats.log | mailx -a /root/backup.log -s "backup-from $DATE" E-MAIL
+cat $LOGFILE | mailx -a /root/backup_filelist.log -s "backup-from $DATE" E-MAIL
 
 #Clean-up
-rm /root/all_databases_$DATE.sql.gz
-rm /$NAME.tar.gz
+rm -f /root/all_databases_$DATE.sql.gz
+rm -f /$NAME.tar.gz
