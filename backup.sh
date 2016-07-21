56b721f586711e489bb9ff65229784aacbb0df03
Son9o <Son9o@users.noreply.github.com>
2016-07-21 17:35:10 +0100

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DATE=`date +%d-%m-%y`
+NAME=`hostname`_$DATE
+cd /
+mysqldump -u root -p$PASSWORD_MYSQL --events --all-databases | gzip > /root/all_databases_$DATE.sql.gz
+tar -cvpzf $NAME.tar.gz --exclude=/$NAME.tar.gz --exclude=/proc --exclude=/sys --exclude=/mnt --exclude=/media --exclude=/run --exclude=/dev --exclude=/lost+found --exclude=/tmp --exclude=/home/son9o/steamcmd --exclude=/home/transmission/Downloads --exclude=/var/lib/transmission/Downloads --exclude=/root/backup.log / > /root/backup.log
+/usr/local/bin/megaput $NAME.tar.gz
+/usr/local/bin/megadf --gb | tail -n3 > /root/megastats.log
+/usr/local/bin/megals -hl | grep -i bridge >> /root/megastats.log
+cat /root/.megarc >> /root/megastats.log
+cat /root/megastats.log | mail -a /root/backup.log -s "backup-from $DATE" E-MAIL
+
+#Clean-up
+rm /root/all_databases_$DATE.sql.gz
+rm /$NAME.tar.gz
