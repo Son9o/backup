9d3f9bf2df1ac61e1b242f2d4b041fc36dfd8519
Son9o <Son9o@users.noreply.github.com>
2016-07-21 17:45:24 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ mysqldump -u root -p$PASSWORD_MYSQL --events --all-databases | gzip > /root/all_
 tar -cvpzf $NAME.tar.gz --exclude=/$NAME.tar.gz --exclude=/proc --exclude=/sys --exclude=/mnt --exclude=/media --exclude=/run --exclude=/dev --exclude=/lost+found --exclude=/tmp --exclude=/home/son9o/steamcmd --exclude=/home/transmission/Downloads --exclude=/var/lib/transmission/Downloads --exclude=/root/backup.log / > /root/backup.log
 /usr/local/bin/megaput $NAME.tar.gz
 /usr/local/bin/megadf --gb | tail -n3 > /root/megastats.log
-/usr/local/bin/megals -hl | grep -i bridge >> /root/megastats.log
+/usr/local/bin/megals -hl | grep -i $NAME >> /root/megastats.log
 cat /root/.megarc >> /root/megastats.log
 cat /root/megastats.log | mailx -a /root/backup.log -s "backup-from $DATE" E-MAIL
 
