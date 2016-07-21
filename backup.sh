e2670e1ab77924a67499c0c20382fcb07d4ba714
Son9o <Son9o@users.noreply.github.com>
2016-07-21 17:58:39 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,8 +6,12 @@ NAME=`hostname`_$DATE
 cd /
 mysqldump -u root -p$PASSWORD_MYSQL --events --all-databases | gzip > /root/all_databases_$DATE.sql.gz
 tar -cvpzf $NAME.tar.gz --exclude=/$NAME.tar.gz --exclude=/proc --exclude=/sys --exclude=/mnt --exclude=/media --exclude=/run --exclude=/dev --exclude=/lost+found --exclude=/tmp --exclude=/home/son9o/steamcmd --exclude=/home/transmission/Downloads --exclude=/var/lib/transmission/Downloads --exclude=/root/backup.log / > /root/backup.log
-/usr/local/bin/megaput $NAME.tar.gz
-/usr/local/bin/megadf --gb | tail -n3 > /root/megastats.log
+echo "Initailised megastats.log on $DATE" > /root/megastats.log
+echo "Output from upload:" >> /root/megastats.log
+/usr/local/bin/megaput $NAME.tar.gz > /root/megastats.log
+echo "Disk space check:" >> /root/megastats.log
+/usr/local/bin/megadf --gb | tail -n3 >> /root/megastats.log
+echo "File List:" >> /root/megastats.log
 /usr/local/bin/megals -hl | grep -i $NAME >> /root/megastats.log
 cat /root/.megarc >> /root/megastats.log
 cat /root/megastats.log | mailx -a /root/backup.log -s "backup-from $DATE" E-MAIL
