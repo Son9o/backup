2120744b91c0fa053a32b05f3237424dbaf0f3a2
Son9o <Son9o@users.noreply.github.com>
2016-07-28 14:38:35 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,10 +4,10 @@
 E_MAIL=$USER@$HOSTNAME
 PASSWORD_MYSQL=your_root_mysql_pass
 DATE=`date +%d-%m-%y-%H-%M`
-BACKUP_PREFIX=backup_
+backup_prefix=backup_
 NAME=`hostname`_$DATE.tar.gz
 #LOGFILE=/root/backup_$NAME.log
-LOGFILE=$HOME/backup_$NAME.log
+LOGFILE=$HOME/$backup_prefix$NAME.log
 BACKUP_TARGET=/root/testdir #What to back-up
 #Just for account creation:
 email_domain=$HOSTNAME
@@ -38,7 +38,7 @@ else
     megaaccount=`awk 'NR==2' $HOME/.megarc | awk '{print $3}'` 
     megaaccountnumber=${megaaccount:7:4}
     ((megaaccountnumber++))
-    MEGA_confirm_key=`megareg --name=backup_$megaaccountnumber --email=backup_$megaaccountnumber@$email_domain --password==$MEGA_password --register --scripted | awk '{print $3}'`
+    MEGA_confirm_key=`megareg --name=$backup_prefix$megaaccountnumber --email=$backup_prefix$megaaccountnumber@$email_domain --password==$MEGA_password --register --scripted | awk '{print $3}'`
     sleep 1m 
     #Below line greps in root mailfolder for mega verification link, this will probably be best adjusted for some other user
     MEGA_confirm_link=`tac $email_drop | grep ^http | grep -m1 confirm`
